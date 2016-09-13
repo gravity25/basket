74c2f8287244fe09f103047b94d3047836abb514
gravity25 <garvitagrawal94@gmail.com>
2016-09-13 18:01:41 +0530

    added indexing

--- models/product.js
+++ models/product.js
@@ -11,6 +11,23 @@ var productSchema = {
 };
 
 var schema = new mongoose.Schema(productSchema);
+
+
+//Creating index
+schema.index({title: 1});
+
+//Mongoose automatically calls ensureIndex for each defined index in your schema.
+//Mongoose will call ensureIndex for each index sequentially,
+//and emit an 'index' event on the model when all the ensureIndex calls succeeded
+//or when there was an error.
+schema.on('index', function (err) {
+  if (err) console.error(err); // error occurred during index creation
+})
+
+// // it is recommended this behavior be disabled in production
+// //since index creation can cause a significant performance impact
+// schema.set('autoIndex', false);  
+
 var Products = mongoose.model('Products', schema);
 
 module.exports = Products;
